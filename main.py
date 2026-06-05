2e938c33a3c34ac46443210dc7d177367666ead5
Aasif Bashir <syeed.ether@gmail.com>
2026-06-05 13:07:30 +0530

    Add ProductDTO model and update product creation and deletion endpoints

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException, Request
 from mockData import products
+from dtos import ProductDTO
 
 app = FastAPI()
 
@@ -11,7 +12,7 @@ def home_page():
 def greet(request: Request):
     queryParams = dict(request.query_params)
     return {
-        "greet": f"your name is {queryParams.get("name")} and your age is {queryParams.get("age")}"
+        "greet": f"your name is {queryParams.get('name')} and your age is {queryParams.get('age')}"
     }
 
 
@@ -25,4 +26,20 @@ def get_product(product_id:int):
     for oneProduct in products:
         if oneProduct.get("id") == product_id:
             return oneProduct
-    return HTTPException(status_code=404, detail="Product not fount")
+    raise HTTPException(status_code=404, detail="Product not found")
+
+
+@app.post("/create_product")
+def create_product(product_data: ProductDTO):
+    product_data = product_data.model_dump()
+    products.append(product_data)
+    return {"detail": "Product created successfully"}
+
+@app.delete("/product/product_id")
+def delete_product(product_id):
+    for index, oneProduct in enumerate(products):
+        if oneProduct.get("id") == product_id:
+            deletedProduct= products.pop(index)
+            return {
+             f'success {deletedProduct}'
+            }
